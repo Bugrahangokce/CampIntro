6f16d9f69008318d48eadb54afa5581c920c2351
90539 <90539@DESKTOP-VVRUJOL>
2022-08-10 23:45:08 +0300

    10 Ağustos Gün Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Product //Entity
+    {
+        public int Id { get; set; }//Primary key
+        public int CategoryId { get; set; }//Referans anahtarı
+        public string ProductName { get; set; }
+        public double UnitePrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        
+
+
+    }
+}
