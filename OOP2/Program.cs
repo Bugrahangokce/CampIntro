6790e76a6ee9c2ee5195a5d456adc79402892d09
90539 <90539@DESKTOP-VVRUJOL>
2022-08-14 01:54:42 +0300

    14 Ağustos Gün Sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IndividualCustomer individualCustomer1 = new IndividualCustomer();
+            individualCustomer1.Id = 1;
+            individualCustomer1.CustomerNumber = "12345";
+            individualCustomer1.FirstName = "Buğrahan";
+            individualCustomer1.LastName = "Gökçe";
+            individualCustomer1.TcNumber = "1234567890";
+
+            //Kodlama.io
+
+            CorporateCustomer corporateCustomer1 = new CorporateCustomer();
+            corporateCustomer1.Id = 2;
+            corporateCustomer1.CustomerNumber = "54321";
+            corporateCustomer1.CompanyName = "Kodlama.io";
+            corporateCustomer1.TaxNumber = "1234567890";
+
+            Customer customer1 = new Customer();
+            Customer customer2 = new CorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+
+            Console.ReadLine();
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLID
+        }
+    }
+}
