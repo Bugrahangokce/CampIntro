b44aab1ff43e315325eee4ab402a16ea869f0a25
90539 <90539@DESKTOP-VVRUJOL>
2022-08-14 20:43:01 +0300

    14 Ağustos Gün Sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreditManager needCreditManager = new NeedCreditManager();        
+            ICreditManager vehicleCreditManager = new VehicleCreditManager();           
+            ICreditManager housingCreditManager = new HousingCreditManager();
+            
+            ApplicationManager applicationManager = new ApplicationManager();
+            applicationManager.MakeAnApplication(needCreditManager);
+
+            Console.WriteLine("------");
+
+            List<ICreditManager> credits = new List<ICreditManager>() {needCreditManager, vehicleCreditManager };
+
+            applicationManager.MakeALoanPreInformation(credits);
+
+            Console.ReadLine();
+        }
+    }
+}
